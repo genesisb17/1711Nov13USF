5cb5206087246d7a341456c493af265a832c4e3c
Yosuf ElSaadany <jo.sadany@gmail.com>
2017-11-14 13:56:53 -0500

    Commit Changes

---
+++ Week 1/HelloWorld/src/day2demo/ControlFlow.java
@@ -0,0 +1,87 @@
+package day2demo;
+
+import java.util.Scanner;
+
+public class ControlFlow {
+
+	/* breaks the flow of execution by decision making
+	 */
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Enter a number:");
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		
+		// IF-STATEMENT
+		if(x < 10) 
+			System.out.println("Hello");
+		else if(x < 100) 
+			System.out.println("World");
+		else 
+			System.out.println("Goodbye");
+		
+		// Ternary
+		System.out.println(x < 10 ? "Hello": "Goodbye");
+		
+		// While
+		while(x < 5) {
+			x++;
+			if(x == 3) continue;
+			System.out.println(x);
+		}
+		
+		do {
+			System.out.println("DO-WHILE STATMENT: " + x);
+			x++;
+		} while(x < 5);
+		
+		// FOR-LOOP
+		for(int i = 0; i < x; i++) {
+			System.out.println("WOOOOOO " + i);
+		}
+		
+		/*
+		 * 1. initialization statement executes
+		 * 2. if booleanexp == true, else exit loop
+		 * 3. execute body
+		 * 4. execute update statement
+		 * 5. return to 2
+		 */
+		
+		// FOR-EACH AKA  ENHANCED FOR LOOP
+		int[] nums = {2,4,5,7,2,251,53,13};
+		int sum = 0;
+		for(int y : nums) {
+			sum += y;
+		}
+		System.out.println(sum);
+		
+		
+		// SWITCH
+		/*
+		 * can hold int, byte, short, char, String, enum
+		 */
+		switch(x) {
+			case 1:
+				System.out.println("x is 1");
+				break;
+			case 5:
+				System.out.println("x is 5");
+				break;
+			case 10:
+				System.out.println("x is 10");
+				break;
+			default:
+				System.out.println("Error");
+		}
+			
+		
+		Operations op = Operations.ADD;
+		System.out.println(op.calculate(10, 10));
+		
+		
+			
+	}
+
+}
