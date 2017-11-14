fa964e00479869b8b4f56f031e6379c02ea8ae31
Nahom Tsadu <nahomtsadu@gmail.com>
2017-11-14 16:00:54 -0500

    Implemented in-class Constructors Demo and Exceptions Demo.
    Signed-off-by: Nahom Tsadu <nahomtsadu@gmail.com>

---
+++ Nahom_Tsadu_Code/Week1/HelloWorld/src/com/revature/hello/ConstructorDemo.java
@@ -0,0 +1,16 @@
+package com.revature.hello;
+
+public class ConstructorDemo {
+
+	int x;
+	int y;
+	int a, b;
+	
+	public ConstructorDemo(){
+		
+	}
+	
+	public ConstructorDemo(int x){
+		this.x = x;
+	}
+}
