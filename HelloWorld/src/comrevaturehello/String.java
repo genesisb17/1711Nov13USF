7708415cb23b488d395a1231c5dd18781d9f3dc1
Trent Smith <smithtrent00@gmail.com>
2017-11-14 06:17:13 -0800

    testing 123

---
+++ HelloWorld/src/comrevaturehello/String.java
@@ -0,0 +1,20 @@
+package comrevaturehello;
+
+public class String {
+	public static void main(String args[])
+	{
+		/*literal - a number presented in code.
+		an int value with a whole number literal. In the following example. 98323559823 is out of range
+		for an int, so we get a compiler error 
+		*/
+		long rightlong =98323559823L;
+		long okToCast = 13415;
+		System.out.print(Long.MAX_VALUE);
+		System.out.print(Long.MIN_VALUE);
+
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		long  rightong = 983235559832L;
+		long okTocast =13415;
+	}
+}
