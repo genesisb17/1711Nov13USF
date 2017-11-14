66599ffb6d87cf85050a7b7f9363a86cd10eb43f
RonyAlvarez <ronyalvarez1993@gmail.com>
2017-11-14 13:04:33 -0500

    minor changes

---
+++ Rony_Alvarez_Code/Week 1/HelloWorld/src/com/revature/hello/UnderstandingExceptions.java
@@ -0,0 +1,37 @@
+package com.revature.hello;
+
+public class UnderstandingExceptions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = new int[4];
+		
+		try {
+			arr[0] = 1;
+			arr[6] = 6;
+		}catch(RuntimeException re) {
+			re.printStackTrace();
+		}catch(ArrayIndexOutOfBoundsException x) {
+			x.printStackTrace();
+		}
+		System.out.println("passed exception");
+		
+		
+	}
+	
+	
+	/*
+	 * Implement the following
+	 * binary search
+	 * breadth first search
+	 * depth first search
+	 * bubble sort
+	 * merge sort
+	 * insertion sort
+	 * 
+	 * 
+	 */
+	
+
+}
