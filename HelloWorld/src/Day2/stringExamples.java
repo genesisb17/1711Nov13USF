ee6bae827040b7be5656db7d827eee42daba0ba7
Trent Smith <smithtrent00@gmail.com>
2017-11-14 07:11:24 -0800

    hello

---
+++ HelloWorld/src/Day2/stringExamples.java
@@ -0,0 +1,20 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class stringExamples {
+	public static void main(String[] args)
+	{
+		String name = "hello ";
+		String trent = "trent";
+		name = name.concat(trent);
+		System.out.println(name);
+		name = name.substring(3);
+		System.out.println(name);
+		boolean var = name.endsWith(trent);
+		System.out.println(var);
+		
+		System.out.println(name.indexOf('t'));
+		System.out.println(name.charAt(0));
+	}
+}
