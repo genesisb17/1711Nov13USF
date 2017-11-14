2d03feacbed5d3115170d982b0e852ef513e97a9
Genesis Bonds <genesis.bonds@revature.com>
2017-11-14 12:08:42 -0500

    class examples

---
+++ Week1/HelloWorld/src/day2demo/AccessLevels.java
@@ -0,0 +1,34 @@
+package day2demo;
+
+/*
+ * outer classes can only be public or package level access. 
+ * 
+ */
+public class AccessLevels {
+	
+	//all levels can be applied to members
+	public int pub;
+	protected int protect;
+	int pack;
+	int priv;
+	
+	// constructors are usually public but can be private, will go into when we use other access mods
+	public AccessLevels(){
+	}
+	
+	
+	protected class ProtectedInner{
+		
+	}
+	
+	private class PrivateInner{
+		
+	}
+
+}
+
+
+class ProtectedClass{
+		
+}
+
