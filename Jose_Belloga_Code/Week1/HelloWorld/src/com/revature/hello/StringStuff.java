2c825cd8d65087a7fc01ff4784d308e4c6cb5444
jgbelloga <jgbelloga3380@eagle.fgcu.edu>
2017-11-14 09:38:43 -0500

    formatting

--- Jose_Belloga_Code/Week1/HelloWorld/src/com/revature/hello/StringStuff.java
+++ Jose_Belloga_Code/Week1/HelloWorld/src/com/revature/hello/StringStuff.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 public class StringStuff {
 	
 	
-public static void main(String[] args) {
+	public static void main(String[] args) {
 	
-	System.out.println("Please enter a string: ");
+		System.out.println("Please enter a string: ");
 	
-	Scanner in = new Scanner(System.in);
-	String myString = in.nextLine();
+		Scanner in = new Scanner(System.in);
+		String myString = in.nextLine();
 	
-	System.out.println("The input string in lowercase: " + 	myString.toLowerCase());
+		System.out.println("The input string in lowercase: " + 	myString.toLowerCase());
 	
-	System.out.println("The length of the string is " + myString.length() + " characters.");
+		System.out.println("The length of the string is " + myString.length() + " characters.");
 	
-	System.out.println("The first character of the string is " + myString.charAt(0) + ".");
+		System.out.println("The first character of the string is " + myString.charAt(0) + ".");
 	
-	System.out.println("Is the string you typed 'cat'?: " + myString.equals("cat"));
-}
+		System.out.println("Is the string you typed 'cat'?: " + myString.equals("cat"));
+	
+		//TODO: manually reverse a string 
+	}
 
 	
 }
