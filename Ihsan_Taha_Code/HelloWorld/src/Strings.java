a70a7128f25fe4bf8f0a317ecb5653ca05938abd
Ihsan Taha <ihsan_taha@yahoo.com>
2017-11-14 18:33:46 -0800

    Adding a new Strings.java file with minimum requirements met unlike previous one

--- Ihsan_Taha_Code/HelloWorld/src/Strings.java
+++ Ihsan_Taha_Code/HelloWorld/src/Strings.java
@@ -1,33 +1,80 @@
-
+/*
+ * This program demonstrates the use of string methods
+ */
 public class Strings {
 	public static void main(String[] args)
 	{
-		String a = "hello";
-		String b = "Hello";
-		String c = new String("hello");
-		String d = a;
-		String e = "hello";
+		String s1 = "str1", s2 = "str2", s3 = "str3";
 		
-		System.out.println("1 " + a.equals(c));
-		System.out.println("2 " + (a==c));
-		System.out.println("3 " + a.equals(e));
-		System.out.println("4 " + (a==d));
-		System.out.println("5 " + a.equalsIgnoreCase(b));
-		System.out.println("6 " + (c==e));
+		// 1. Returns the char at the given index
+		char c = s1.charAt(0);
+			
+		// 2. Returns the substring of the string starting from the given index to the end
+		int i = 1;
+		String s4 = s1.substring(i);
 		
-		System.out.println(test(a));
-		System.out.println(a);
-		test2(a);
-		System.out.println(a);
+		// 3. Returns an int based on whether the given strings are equal
+		String s5 = s1;
+		int f = s1.compareTo(s2), t = s1.compareTo(s5);
+		
+		// 4. Returns the concatenation of the given strings
+		String s6 = s1.concat(s2);
+		
+		// 5. Returns a string based on the copied char array
+		char[] cArray = {'c','h','a','r',' ','a','r','r','a','y'};
+		String s7 = String.copyValueOf(cArray);
+		
+		// Prints the results of all given methods
+		printString(1, s1);
+		printString(2, s2);
+		printString(3, s3);
+		
+		System.out.println("\n");
+		
+		printString(i, s1, c);
+		printString(4, s4);
+		printString(s1, s2, f);
+		printString(s1, s5, t);
+		printString(s1, s2, s6);
+		printString(cArray, s7);
 	}
 	
-	static String test(String x)
+	
+	
+	// Overloaded method for displaying results of the string methods
+	public static void printString(int i, String s, char c)
 	{
-		return x.substring(2);
+		System.out.println("The char at index " + (i-1) + " of " + s + " is: " + c);
 	}
 	
-	static void test2(String x)
+	public static void printString(int i, String str)
 	{
-		System.out.println(x.substring(2));
+		System.out.println("String " + i + ":\t" + str);
+	}
+	
+	public static void printString(String s1, String s2, int i)
+	{
+		System.out.println("Strings " + s1 + " and " + s2 + " are equal? " + i);
+	}
+	
+	public static void printString(String s1, String s2, String sConcat)
+	{
+		System.out.println("The concatenation of " + s1 + " and " + s2 + " is: " + sConcat);
+	}
+	
+	public static void printString(char[] cArray, String s)
+	{
+		System.out.println("The string of the char array '" + getString(cArray) + "' is: " + s);
+	}
+	
+	public static String getString(char[] cArray)
+	{
+		String s = "";
+		for (int i = 0; i < cArray.length; i++)
+		{
+			s += cArray[i];
+		}
+		
+		return s;
 	}
 }
