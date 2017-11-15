872c16e06bc1ce225b2ed13ff5e8754b2dac505e
Tyler Ehringer <tyler.ehringer@gmail.com>
2017-11-15 08:49:09 -0500

    Adding Stringa.java

--- Tyler_Ehringer_Code/Week 1/String Examples/src/p1/Stringa.java
+++ Tyler_Ehringer_Code/Week 1/String Examples/src/p1/Stringa.java
@@ -6,10 +6,11 @@ public class Stringa{
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String example = s.nextLine();
-		System.out.println(example.endsWith("a"));
-		System.out.println(example.toUpperCase());
-		System.out.println(example.replace('e', 'r'));
-		System.out.println(example.length());
-		System.out.println(example.concat(example));
+		System.out.println(example.endsWith("a")); //checks if the string ends with an 'a'
+		System.out.println(example.toUpperCase()); // converts string to upper case
+		System.out.println(example.replace('e', 'r')); // replaces 'e's with 'r's
+		System.out.println(example.length()); // prints the length of the string
+		System.out.println(example.concat(example)); //concats the string with itself
+		
 	}
 }
