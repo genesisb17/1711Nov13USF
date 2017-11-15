e79fe3a689d21d8dc3faedcb3362a495b2e84dfc
Wezley Singleton <wezley.singleton@gmail.com>
2017-11-14 22:35:10 -0500

    figured out binary sort implementation

--- week_1/src/day2demo/SearchAndSorts.java
+++ week_1/src/day2demo/SearchAndSorts.java
@@ -23,50 +23,73 @@ public class SearchAndSorts {
 		String userInput = scan.nextLine();
 		
 		String[] userArr = userInput.split(" ");
-		int[] numArr = new int[userArr.length];
+		int[] intArr = new int[userArr.length];
 		
-		for(int i = 0; i < numArr.length; i++) {
-			numArr[i] = Integer.parseInt(userArr[i]);
+		for(int i = 0; i < intArr.length; i++) {
+			intArr[i] = Integer.parseInt(userArr[i]);
 		}
 		
-		binarySearch(13, numArr);
+		binarySearch(13, intArr);
 		scan.close();
 	}
 	
-	static void binarySearch(int value, int... nums) {
-	
-		// sorts the given array and prints it to the console
-		Arrays.sort(nums);
-		System.out.print("\nThe sorted array of integers given: ");
-		for(int i = 0; i < nums.length; i++) {
-			System.out.print(nums[i]);
-			System.out.print(" ");
-		}
+	static void binarySearch(int value, int... intArr) {
 		
-		// determines the midpoint index position of the given array
-		int midpoint = nums.length / 2;
-		
-		// conditionals to determine the location of the desired value
-		if (nums[midpoint] == value) {
-			System.out.println("\nResult: " + value);
-		}
-		
-		else if (nums[midpoint] > value) {
+		// sort the given array in ascending order, print to confirm
+		Arrays.sort(intArr);
+		System.out.print("\nYour values, sorted in ascending order: {");
+		for (int i = 0; i < intArr.length; i++) {
 			
-			int[] x = Arrays.copyOfRange(nums, 0, midpoint);
-			binarySearch(value, x);
+			if (i == intArr.length - 1) {
+				System.out.print(intArr[i]);
+				System.out.print("}\n");
+			}
+			else {
+				System.out.print(intArr[i] + ", ");
+			}
 			
 		}
 		
-		else if (nums[midpoint] < value) {
+		// represents the first element of the given substring
+		int p = 0;
+		
+		// represents the last element of the given substring
+		int r = intArr.length;
+		
+		// using a while loop we can iterate over the following steps to determine if the value is in the array
+		while (p <= r) {
 			
-			int[] y = Arrays.copyOfRange(nums, (midpoint), (nums.length - 1));
-			binarySearch(value, y);
-		
-		}
-		
-		else {
-			System.out.println("The value is not present within the given array.");
+			/* 
+			 *  establish the midpoint index position of the given substring, using the first and last 
+			 *  considered index positions
+			 */
+			int midpoint = (p + r) / 2;
+			
+			// if the value of the midpoint index position is equal to the sought value, print success.
+			if (intArr[midpoint] == value) {
+				System.out.println("\nThe element within the array, with a value of " + value
+						+ " is located at index position " + midpoint + ".");
+				
+				// causes the while loop to discontinue
+				p = 0; 
+				r = -1;
+			}
+			
+			/*
+			 *  if the value of the midpoint index position is less than the sought value, shift the last 
+			 *  element to the index position before the current midpoint index position.
+			 */
+			else if (intArr[midpoint] > value) {
+				r = midpoint - 1;
+			}
+			
+			/*
+			 *  if the value of the midpoint index position is greater than the sought value, shift the first 
+			 *  element to the index position after the current midpoint index position.
+			 */
+			else if (intArr[midpoint] < value) {
+				p = midpoint + 1;
+			}
 		}
 		
 	}
