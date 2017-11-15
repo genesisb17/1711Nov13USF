e7d4c8af6eea42135741970938c3f15cc42df256
Cameron Jackson <jackson1123@gmail.com>
2017-11-15 14:56:15 -0500

    Added students to student.txt

--- Cameron_Jackson_Code/Week1/HelloWorld/src/com/revature/io/StudentIO.java
+++ Cameron_Jackson_Code/Week1/HelloWorld/src/com/revature/io/StudentIO.java
@@ -1,5 +1,24 @@
 package com.revature.io;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class StudentIO {
 
+	static String filename = "src/logs/students.txt";
+	public static void main(String[] args) {
+		Student s = new Student("Billy-Bob Thornton", 52);
+		writeStudent(s);
+	}
+	
+	static void writeStudent(Student student) {
+		//Try-with-resources
+		try (BufferedWriter bw = 
+				new BufferedWriter(new FileWriter(filename, true))) {
+			bw.write(student.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
