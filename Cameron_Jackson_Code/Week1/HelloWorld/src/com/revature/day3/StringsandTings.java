8677301a4e72583a04a3081c08c766c84ffda069
Cameron Jackson <jackson1123@gmail.com>
2017-11-15 18:58:44 -0500

    added garbage collection call and runtime obj to StringsAndTings

--- Cameron_Jackson_Code/Week1/HelloWorld/src/com/revature/day3/StringsandTings.java
+++ Cameron_Jackson_Code/Week1/HelloWorld/src/com/revature/day3/StringsandTings.java
@@ -17,15 +17,27 @@ public class StringsandTings {
 		System.out.println(buff.reverse());
 		
 		// DAY 3 ASSIGNMENT
+		/* StringBuilder calls */
 		StringBuilder sbstr = new StringBuilder("Bob's your uncle");
 		sbstr.append(". Punctuation matters.");
 		sbstr.reverse();
 		sbstr.insert(5, " pardon me ");
 		
+		/* StringTokenizer */
 		StringTokenizer strtok = new StringTokenizer("comma:separated:list:of:things", ":");
 		while (strtok.hasMoreTokens()) {
 			System.out.println(strtok.nextToken());
 		}
+		
+		/* Requesting garbage collection */
+		System.gc();
+		
+		/* Runtime object */
+		Runtime rt = Runtime.getRuntime();
+		rt.gc(); // can request garbage collection this way as well
+		System.out.println("Free mem: " + rt.freeMemory() + "bytes"); // print the amount of memory remaining in JVM
+		// rt.exec(command); can be used to call other commands in a separate process
+		// rt.halt(); can be used to forcibly stop the JVM
 	}
 
 }
