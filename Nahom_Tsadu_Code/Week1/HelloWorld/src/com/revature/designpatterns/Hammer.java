6814d21d4b54f12f8ad15457c89221595368cde0
Nahom Tsadu <nahomtsadu@gmail.com>
2017-11-16 16:25:45 -0500

    Implemented ToolFactory Design Pattern Demo
    Signed-off-by: Nahom Tsadu <nahomtsadu@gmail.com>

---
+++ Nahom_Tsadu_Code/Week1/HelloWorld/src/com/revature/designpatterns/Hammer.java
@@ -0,0 +1,8 @@
+package com.revature.designpatterns;
+
+public class Hammer implements Tool{
+
+	public String work(){
+		return "The hammer is working!";
+	}
+}
---
+++ Nahom_Tsadu_Code/Week1/HelloWorld/src/com/revature/designpatterns/Wrench.java
@@ -0,0 +1,8 @@
+package com.revature.designpatterns;
+
+public class Wrench implements Tool{
+
+	public String work(){
+		return "The wrench is working!";
+	}
+}
