b08f7615dcdce693609ccc830f6062b56b605779
David Argetsinger <Dargetsinger@islander.tamucc.edu>
2017-11-15 21:52:34 -0500

    String assignment , string tokenizer assignment, string builder assignment, and Runtime assignment

--- David_Argetsinger_Code/Week1/HelloWorld/src/day3demo/StringAssignment.java
+++ David_Argetsinger_Code/Week1/HelloWorld/src/day3demo/StringAssignment.java
@@ -4,6 +4,11 @@ import java.util.StringTokenizer;
 
 public class StringAssignment {
 
+	private static Runtime rt;
+	//Runtime.Process exec (...) allows for multithtreading has multiple overloaded 
+	//Runtime.exit(int) terminates virtual machine, will close the program 
+	//Runtime.halt  will also terminate the machine but will not wait for finalizers to finish ( is dangerous)
+	//Runtime.load(string) used for loading into the dynamic library 
 	public static void main(String[] args) {
 		StringBuilder st= new StringBuilder("yellow");
 		//three methods to manip
@@ -23,6 +28,14 @@ public class StringAssignment {
 //		Create two String objects with number values (i.e. “20). Write a method that adds the two.
 //		Request garbage collection in your method.
 //		Create a Runtime object and note at least three methods. Imagine how you would use them.
+		
+		str="20";
+		String str1="10";
+		int integ=Integer.valueOf(str1)+Integer.valueOf(str);
+		System.out.println(integ);
+		System.gc();
+
+		
 		
 	}
 
