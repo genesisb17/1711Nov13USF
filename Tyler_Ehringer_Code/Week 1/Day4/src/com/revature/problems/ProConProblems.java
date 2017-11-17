51419f96b13d4681fc666e9ba01f342bbc5e73f4
Tyler Ehringer <tyler.ehringer@gmail.com>
2017-11-16 21:51:06 -0500

    Comitting day4 assignments

---
+++ Tyler_Ehringer_Code/Week 1/Day4/src/com/revature/problems/ProConProblems.java
@@ -0,0 +1,88 @@
+package com.revature.problems;
+
+import java.util.Random;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.IntStream;
+
+public class ProConProblems {
+	
+	void createDeadlock() {
+		
+		String s1 = "Resource 1", s2 = "Resource 2";
+		
+		Thread t1 = new Thread(() -> {
+			System.out.println("t1 will now request s1");
+			synchronized(s1) {
+				System.out.println("t1 owns s1");
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("t1 will now request s2");
+				synchronized(s2) {
+					System.out.println("t1 owns s2");
+				}
+			}
+		});
+		Thread t2 = new Thread(() -> {
+			System.out.println("t2 will now request s2");
+			synchronized(s2) {
+				System.out.println("t2 owns s2");
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("t2 will now request s1");
+				synchronized(s1) {
+					System.out.println("t2 owns s1");
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+	}
+	
+	
+	
+	public static String randomString(int minSize, int maxSize) {
+		Random r = new Random();
+		StringBuilder sb = new StringBuilder();
+		IntStream.rangeClosed(0, r.nextInt(maxSize - minSize) + minSize).forEach(i -> sb.append((char)(r.nextInt(26) + 'a')));
+		return sb.toString();
+	}
+	
+	public static void producerConsumer() {
+		LinkedBlockingQueue<String> lbq = new LinkedBlockingQueue<>(32);
+		
+		Thread producer = new Thread(() -> {
+			IntStream.range(0, 100).forEach(i -> {
+				try {
+					lbq.put(randomString(5, 15));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			});
+		});
+		Thread consumer = new Thread(() -> {
+			IntStream.range(0,  100).forEach(i -> {
+				try {
+					System.out.println(lbq.take());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			});
+		});
+		producer.start();
+		consumer.start();
+		
+		
+	}
+
+	public static void main(String[] args) {
+		producerConsumer();
+	}
+	
+}
