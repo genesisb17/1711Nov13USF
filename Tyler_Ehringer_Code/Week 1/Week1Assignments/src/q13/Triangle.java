86e39ab0eaf97b523fceaf184261415ed0d1bbb8
Tyler Ehringer <tyler.ehringer@gmail.com>
2017-11-19 22:18:26 -0500

    Improved q13

--- Tyler_Ehringer_Code/Week 1/Week1Assignments/src/q13/Triangle.java
+++ Tyler_Ehringer_Code/Week 1/Week1Assignments/src/q13/Triangle.java
@@ -1,13 +1,19 @@
 package q13;
 
+import java.util.Iterator;
+import java.util.stream.Stream;
+
 public class Triangle {
-	
+
 	public static void main(String[] args) {
-		String s = "0101010101";
-		int index = 0;
-		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j <= i; j++) {
-				System.out.print(s.charAt(index++));
+		printTriangle(4);
+	}
+
+	public static void printTriangle(int rows) {
+		Iterator<Character> iter = Stream.iterate('0', c -> c == '0' ? '1' : '0').iterator();
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(iter.next());
 			}
 			System.out.println();
 		}
