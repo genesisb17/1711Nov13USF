66be2343ae781b8b1050f4aacb2d64604cde015e
unknown <shujunye94@gmail.com>
2017-11-23 19:58:02 -0500

    add jdbcintro

---
+++ Shujun_Ye_Code/Week2/jdbcintro/src/main/java/com/ex/test/MainTest.java
@@ -0,0 +1,29 @@
+package com.ex.test;
+
+import java.util.ArrayList;
+
+import com.ex.dao.DAO;
+import com.ex.dao.DAOImpl;
+import com.ex.pojos.Artist;
+
+public class MainTest {
+	public static void main(String[] args) {
+		DAO dao = new DAOImpl();
+		
+
+		
+//		Artist art = dao.getArtistById(6);
+//		System.out.println(art);
+		
+//		System.out.println(dao.addArtist("Vic Mensa"));
+		
+//		Artist art = dao.updateArtist(117, "The Ohio Players");
+//		System.out.println(art);
+		
+		ArrayList<Artist> artists = dao.getArtists();
+		
+		for (Artist art : artists) {
+			System.out.println(art);
+		}
+	}
+}
