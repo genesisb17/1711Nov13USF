90617f04b1ec43ee91086cbe0624f1b9bcbf22d1
marshallc000 <marshall.c.000@gmail.com>
2017-12-01 09:45:37 -0500

    work from 11-30

---
+++ Marshall_Cargle_Code/Week3/Bank2/src/main/java/com/ex/servlets/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.ex.servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rev.service.Service;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	static Service service = new Service();
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("In login servlet");
+
+		// 1. get recieved JSON data from request
+		BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+		String json = "";
+		if (br != null) {
+			json = br.readLine();
+		}
+		System.out.println("JSON STRING: " + json);
+
+		// 2. Initiate Jackson Mapper
+		ObjectMapper mapper = new ObjectMapper();
+		
+		// 3. Convert recieved JSON to String array
+		String[] user=mapper.readValue(json, String[].class);
+		String username = user[0];
+		String password = user[1];
+		System.out.println(username);
+		
+		// 4. Set response type to JSON
+		//resp.setContentType("application/json");
+	}
+}
