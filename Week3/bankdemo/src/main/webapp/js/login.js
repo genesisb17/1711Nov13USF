ff63d18c5f2879e50afcc4a9ae5c31b25cef85c9
Genesis Bonds <genesis.bonds@revature.com>
2017-12-02 15:48:57 -0500

    completed login and register functionality

--- Week3/bankdemo/src/main/webapp/js/login.js
+++ Week3/bankdemo/src/main/webapp/js/login.js
@@ -1,8 +1,9 @@
 /**
- * 
+ * previously app.js
  */
 
 window.onload = function(){
+	$('#message').hide();
 	$('#login').on('click',login);
 	$('#register').on('click', register);
 	
@@ -16,33 +17,40 @@ function login(){
 	var toSend = [username, password];
 
 	
+	
 	var json = JSON.stringify(toSend);
 	console.log(json);
 	
 	var xhr = new XMLHttpRequest();
+	console.log(xhr.readyState);
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4 && xhr.status==200){
 			console.log("in xhr callback" + xhr.responseText);
 			var user = JSON.parse(xhr.responseText);
+			$('#message').show();
 			if(user == null){
-				console.log("user DNE");
+				$('#message').html("Invalid user") ;
 			}
 			else if(user.id == 0){
-				console.log("wrong pw");
+				$('#message').html( "Invalid password");
 			}
 			else{
+				$('#message').html(`Welcome ${user.firstname}`) ;
+				window.location.replace('register.html');
 				console.log("success!");
 			}
 		}
 	};
 	
 	xhr.open("POST","login", true);
+	console.log(xhr.readyState);
 	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	console.log("AFTER HEADER " + xhr.readyState);
 	xhr.send(json);
 	
 
 }
 
 function register(){
-	alert("registering");
+	window.location.replace('register.html');
 }
