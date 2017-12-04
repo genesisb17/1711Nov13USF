e2ff3341d767b94389b6250259ffb076668cbbe2
marshallc000 <marshall.c.000@gmail.com>
2017-12-04 09:59:42 -0500

    Javascript Workbook

--- Marshall_Cargle_Code/Week3/HelloWeb/WebContent/js/JavaScriptWorkbookFunctions.js
+++ Marshall_Cargle_Code/Week3/HelloWeb/WebContent/js/JavaScriptWorkbookFunctions.js
@@ -1,6 +1,9 @@
 document.getElementById("doFib").addEventListener("click", runFib);
 document.getElementById("doBub").addEventListener("click", runBub);
 document.getElementById("doRev").addEventListener("click", runRev);
+document.getElementById("doFac").addEventListener("click", runFac);
+document.getElementbyId("doSub").addEventListener("click", runSub);
+document.getElementById("doEven").addEventListener("click", runEven);
 
 function fib(n){
 	console.log(n);
@@ -9,7 +12,6 @@ function fib(n){
 }
 
 function runFib(){
-	//alert("HELLO!");
 	var display = document.getElementById("fibDisplay");
 	var n=document.getElementById("fib").value;
 	display.innerHTML=fib(n);
@@ -46,4 +48,43 @@ function reverse(s){
 	for(let i=0; i<s.length;i++)
 		s=s.substring(1, s.length-i)+s.substring(0,1)+s.substring(s.length-i);
 	return s;
+}
+
+function fac(n){
+	console.log(n);
+	if(n==1) return 1;
+	else return (n*fac(n-1));
+}
+
+function runFac(){
+	var display = document.getElementById("facDisplay");
+	var n=document.getElementById("fac").value;
+	display.innerHTML=fac(n);
+}
+
+function runSub(){
+	var display = document.getElementById("subDisplay");
+	var s=docuemnt.getElementbyId("subStr").value;
+	var n=document.getElementById("subLen").value;
+	//if(typeof(n)=="String")
+		//alert("Wrong value for Substring");
+	var a=document.getElementById("subOff").value;
+	//if(typeof(a)=="String")
+		//alert("Wrong value for Substring");
+	display.innerHTML=sub(s,n,a);
+}
+
+function sub(s,n,a){
+	return s.substring(n, n+a);
+}
+
+function runEven(){
+	//var display = document.getElementById("evenDisplay");
+	//var n=document.getElementById("eve").value;
+	//display.innerHTML=even(n);
+	alert(even(5));
+}
+
+function even(n){
+	return 2 % 0 ==0;
 }
