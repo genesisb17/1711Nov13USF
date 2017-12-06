e02ea96dae18f660eb641abe438cf33148a8821e
Genesis Bonds <genesis.bonds@revature.com>
2017-12-06 11:55:59 -0500

    fixing front controller

--- Week3-4/bankdemo/src/main/java/com/ex/servlets/GetViewsServlet.java
+++ Week3-4/bankdemo/src/main/java/com/ex/servlets/GetViewsServlet.java
@@ -23,6 +23,10 @@ public class GetViewsServlet extends HttpServlet {
 		String nextView = new RequestHelper().process(req, resp);
 		
 		req.getRequestDispatcher(nextView).forward(req, resp);
+		
+
+//		req.getRequestDispatcher("partials/home.html")
+//		.forward(req, resp);
 	}
 
 	
