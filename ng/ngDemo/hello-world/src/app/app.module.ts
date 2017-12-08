2a0261c784dd0d2ee9f66cf1514febf4c1dea053
Genesis Bonds <genesis.bonds@gmail.com>
2017-12-07 22:25:55 -0500

    more things from today

--- ng/ngDemo/hello-world/src/app/app.module.ts
+++ ng/ngDemo/hello-world/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { CoursesService } from './course/courses.service';
 
 
 @NgModule({
@@ -16,7 +17,9 @@ import { CourseComponent } from './course/course.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ng/ngDemo/hello-world/src/app/courses.component.ts
+++ ng/ngDemo/hello-world/src/app/courses.component.ts
@@ -1,12 +1,23 @@
 import {Component} from '@angular/core';
+import { CoursesService } from './course/courses.service';
 
 @Component({
     //properties to tell Angular how the component works
     selector: 'courses', //uses css selectors ; with components we can extend HTML vocab. we're trying to define an element named 'courses'
-    template: '<h2>{{title}}</h2>' //html markup
+    template: `
+        <h2>{{ getTitle()}}</h2> 
+        <ul>
+            <li *ngFor="let course of courses">{{course}}</li> 
+        </ul>` //html markup; string interpolation 
+        // we use directives "ng-" to manipulate the dom
     //expressions are evaluated at runtime; data binding; we are binding our view with our component
 })
 export class CoursesComponent{
-    title = "List of course";
+    title = "List of courses";
+    courses;
 
+    constructor(service: CoursesService){ //this constructor has a service dependency of type CoursesService
+        this.courses = service.getCourses();
+    }
+   
 }
