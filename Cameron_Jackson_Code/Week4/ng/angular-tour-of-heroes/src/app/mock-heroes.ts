eb51d2ab970288748310eeaa87a1929592ec6d4c
Cameron Jackson <jackson1123@gmail.com>
2017-12-11 12:39:23 -0500

    Commiting before breaking tour of heroes

---
+++ Cameron_Jackson_Code/Week4/ng/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Wind Dude'},
+    { id: 2, name: 'The Incredible Guy' },
+    { id: 3, name: 'Repair Man Man' },
+    { id: 4, name: 'Metal-Face Doom' },
+    { id: 5, name: 'Spider-Person' },
+    { id: 6, name: 'Cat-Man' },
+    { id: 7, name: 'Spawn' },
+    { id: 8, name: 'Wave Rider' },
+    { id: 9, name: 'Silver Surfer' },
+    { id: 10, name: 'The Hand' },
+    { id: 11, name: 'Crazy Diamond' }
+];
