d7068327a6f1bfc891bc7a90207efe5f649273fe
Genesis Bonds <genesis.bonds@gmail.com>
2017-12-12 16:49:17 -0500

    push

--- Week3-4/todo-app/src/app/app.component.ts
+++ Week3-4/todo-app/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
- // template: `<h2> hi, {{ name }}
-  // {{title}} </h2>` ,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app';
-  name = "gen";
+    newTodo: Todo = new Todo();
+  /*
+    Ask DI system to inject the dependency associated 
+    with our DI token 'tododataservice' and assign it a property.
+  */
+  constructor(private todoDataService: TodoDataService){}
+
+  addTodo(){
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  get todos(){
+    return this.todoDataService.getAllTodos();
+  }
+
+
 }
--- Week3-4/todo-app/src/app/todo.ts
+++ Week3-4/todo-app/src/app/todo.ts
@@ -4,6 +4,15 @@ export class Todo {
     title: string ='';
     complete: boolean = false;
 
+    /*  We are adding constructor logic that lets us specify property values
+    during instantiation so that we can easily create new todo instances like:
+
+        let todo = new Todo({
+            title:'Finish Project 1',
+            complete: false
+        });
+
+    */
     constructor(values: Object = {}){
         Object.assign(this,values);
     }
