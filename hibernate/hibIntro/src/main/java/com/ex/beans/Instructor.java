eed84b9aec8b32cf51ed42d7d687f759d39645ff
Genesis Bonds <genesis.bonds@gmail.com>
2017-12-16 17:33:26 -0500

    more hibernate

--- hibernate/hibIntro/src/main/java/com/ex/beans/Instructor.java
+++ hibernate/hibIntro/src/main/java/com/ex/beans/Instructor.java
@@ -8,46 +8,88 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.NamedNativeQueries;
+import org.hibernate.annotations.NamedNativeQuery;
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
+
 @Entity
-@Table(name="HIP_INSTRUCTORS")
+/*
+ * Named Queries
+ * Hard to maintain and ugly. avoid, but here's what 
+ * named queries are. 
+ * This can be done via XML or annotations. Below is how 
+ * it works with annotations
+ * for further research/info on how it works in your 
+ * Hibernate mapping file, check out:
+ * http://www.mkyong.com/hibernate/hibernate-named-query-examples/
+ * 
+ * Check HibDAO for how these are called
+ 
+//In HQL
+@NamedQueries({
+	@NamedQuery(
+			name="findInstructorByIdHQL",
+			query="from Instructor i where i.id = :id"
+			),
+	@NamedQuery(
+			name="findInstructorByNameHQL",
+			query="from Instructor i where name like :name"
+			)
+})
+//In native SQL
+@NamedNativeQueries({
+	@NamedNativeQuery(
+			name="findInstructorByIdSQL",
+			query="select * from instructors where inst_id = :id"
+			),
+	@NamedNativeQuery(
+			name="findInstructorsByNameSQL",
+			query="select * from instructors where instructor_name like :name"
+			)
+})*/
+
+@Table(name="INSTRUCTORS")
 public class Instructor {
 	
 	@Id
-	@Column(name="INSTRUCTOR_ID")
+	@Column(name="INST_ID")
 	@SequenceGenerator(name="INST_ID_SEQ", sequenceName="INST_ID_SEQ")
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="INST_ID_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="INST_ID_SEQ")
 	private int id;
 	
-	@Column(name="INSTRUCTOR_NAME", nullable=false)
+	@Column(name="INSTRUCTOR_NAME")
 	private String name;
+
+	public Instructor(){}
 	
-	
-	
-	public Instructor() {}
-	public Instructor(String name) {
-		this.name=name;
-	}
 	public Instructor(int id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public String toString() {
 		return "Instructor [id=" + id + ", name=" + name + "]";
 	}
+	
 	
 	
 	
--- hibernate/hibIntro/src/main/java/com/ex/dao/HibDao.java
+++ hibernate/hibIntro/src/main/java/com/ex/dao/HibDao.java
@@ -1,19 +1,23 @@
 package com.ex.dao;
 
 import java.util.List;
+import java.util.Set;
 
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
+import com.ex.beans.Course;
+import com.ex.beans.Instructor;
 import com.ex.beans.Student;
 import com.ex.beans.Transcript;
 import com.ex.util.ConnectionUtil;
 
 public class HibDao {
-	
+	//CREATE
 	public Student addStudent(Student student) {
 		Session session = ConnectionUtil.getSession();
 		Transcript script = new Transcript();
@@ -36,6 +40,101 @@ public class HibDao {
 		return student;
 		
 	}
+	public void addInstructor(Instructor instructor){
+		Session session = ConnectionUtil.getSession();
+
+		try{
+			Transaction tx = (Transaction) session.beginTransaction();
+			session.save(instructor);
+			tx.commit();
+		}
+		finally{
+			session.close();
+		}
+	}
+
+	public Course addCourse(Course c){
+		Session session = ConnectionUtil.getSession();
+		Course ret = null;
+		Transaction tx = (Transaction) session.beginTransaction();
+		
+		try{
+			int id = (Integer)session.save(c);
+			c.setId(id);
+			tx.commit();
+			ret = c;
+		}
+		catch(RuntimeException e){
+			/*
+			 *  Looking into hibernate transaction commands
+			 *  In Hibernate, tx management is difficult, so 
+			 *  remember that any exceptions thrown are FATAL, 
+			 *  and you have to roll back the transaction and
+			 *  close the current session immediately 
+			 */
+			try{
+				tx.rollback();
+			}catch(RuntimeException re){
+				re.printStackTrace();
+			}
+		}
+		finally{
+			session.close();
+		}
+		return ret;
+	}
+
+	
+	//READ *********************************************
+		/*
+		 * Often, we will see Hibernate applications that use a mix
+		 * of session.get() and session.load(). Both are used to 
+		 * retrieve objects, just with different mechanisms
+		 * for more, see: http://www.mkyong.com/hibernate/different-between-session-get-and-session-load/
+		 */
+
+		/* session.get() will always hit the database and return the 
+		 * real object (an object that represents a row in the db),
+		 * not a proxy.
+		 * if the row isnt found, get() will return null
+		 */
+		public Instructor getInstructorByID(int id){
+			Session session = ConnectionUtil.getSession();
+			Instructor i = null;
+			try{
+				i = (Instructor) session.get(Instructor.class, id);
+			}
+			catch(HibernateException e){
+				e.printStackTrace();
+			}
+			finally{
+				session.close();
+			}
+			return i;
+		}
+
+		/*
+		 * session.load() will always return a "proxy"(Hibernate
+		 * term) without hitting the database. In Hibernate, a proxy is 
+		 * an object with the given identifier value; its properties
+		 * are not initialized yet. It looks like a temporary fake object
+		 * If no row is found, it throws an ObjectNotFoundException, 
+		 * so only use this if you're sure the object exists
+		 */
+		public Instructor loadInstructorByID(int id){
+			Session session = ConnectionUtil.getSession();
+			Instructor i = null;
+			try{
+				i = (Instructor) session.load(Instructor.class, id);
+			}
+			catch(HibernateException e){
+				e.printStackTrace();
+			}
+			finally{
+				session.close();
+			}
+			return i;
+		}
 	
 	/*
 	 * CRITERIA API
@@ -82,7 +181,57 @@ public class HibDao {
 		
 	}
 	
-	
+	//calling an HQL named query example
+		public List<Instructor> getNamesLike(String like){
+			Session session = ConnectionUtil.getSession();
+			Query query = session.getNamedQuery("findInstructorByNameHQL")
+					.setString("name", like);
+			List<Instructor> i= query.list();
+			return i;
+		}
+
+		//calling native sql named query. same syntax
+		public Instructor getInstructorById(int id){
+			Session session = ConnectionUtil.getSession();
+			Query query = session.getNamedQuery("findInstructorByIdSQL")
+					.setInteger("id", id);
+			Instructor i = (Instructor) query.list().get(0);
+			return i;
+		}
+
+
+
+		//UPDATE
+		public void addCourseToTranscript(Transcript t, Course c){
+			Set<Course> courses = t.getCourses();
+			if(courses != null){
+				for(Course temp :courses){
+					courses.add(temp);
+				} 
+			}
+			courses.add(c);
+
+			t.setCourses(courses);
+			Session session = ConnectionUtil.getSession();
+			try{
+				Transaction tx = session.beginTransaction();
+				session.update(t);
+				tx.commit();
+			}
+			finally{
+				session.close();
+			}
+		}
+
+		
+		//DELETE
+		public void deleteInstructorByIdQuery(int id){
+			Session session = ConnectionUtil.getSession();
+			Query query = session.createQuery("delete Instructor where id = :id");
+			query.setParameter("id",id);
+			int ret = query.executeUpdate();
+			System.out.println(ret + " Rows Deleted");
+		}
 	
 
 }
