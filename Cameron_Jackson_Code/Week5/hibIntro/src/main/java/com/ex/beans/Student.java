a393d59e5b3189ed14bace46969b5a08288a66ee
Cameron Jackson <jackson1123@gmail.com>
2017-12-19 17:07:49 -0500

    updates

--- Cameron_Jackson_Code/Week5/hibIntro/src/main/java/com/ex/beans/Student.java
+++ Cameron_Jackson_Code/Week5/hibIntro/src/main/java/com/ex/beans/Student.java
@@ -12,8 +12,12 @@ import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
 @Table(name="HIP_STUDENTS")
+@Cache(usage=CacheConcurrencyStrategy.READ_WRITE, region="student")
 public class Student {
 	
 	@Id
