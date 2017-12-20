d0cc5b1685c438e387f45a8cfd16dc6aadfec2a4
Genesis Bonds <genesis.bonds@gmail.com>
2017-12-20 09:32:07 -0500

    Spring intro example

---
+++ Spring/springdemo/src/main/java/com/ex/main/MainApp.java
@@ -0,0 +1,36 @@
+package com.ex.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ex.beans.HelloWorld;
+
+public class MainApp {
+	/*
+	 * Spring DI: Dependency Injection
+	 * 
+	 * Spring is going to hold our dependencies (our objects) 
+	 * 	and inject them where need (apply new object())
+	 * 
+	 * What is needed for Spring to do DI?
+	 * 	 Configure the spring container
+	 * 		-by xml
+	 * 		-by @s
+	 * 		-This example will be using XML 
+	 * 
+	 * How instantiate a spring container?
+	 * 		-beanFactory (old) the parent class, lazy loads beans
+	 *		-applicationContext (new) the child class, eagerly loads beans
+	 */
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("beans.xml");
+		
+		HelloWorld world = (HelloWorld) context.getBean("helloWorld");
+		world.getMessage();
+		world.getTest();
+	}
+
+}
