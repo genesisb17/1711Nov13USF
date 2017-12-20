dfcb26e5594ad6734e5b86050a6eb80767e73f37
Genesis Bonds <genesis.bonds@gmail.com>
2017-12-20 16:02:28 -0500

    finishing orm bear demo

--- Spring/springdemo/src/main/java/com/ex/orm/Bear.java
+++ Spring/springdemo/src/main/java/com/ex/orm/Bear.java
@@ -2,6 +2,8 @@ package com.ex.orm;
 
 import java.io.Serializable;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,59 +17,54 @@ import org.springframework.stereotype.Component;
 @Component
 @Entity
 @Table(name="BEARS")
-public class Bear implements Serializable{
+@AttributeOverrides({
+	@AttributeOverride(name="id", column=@Column(name="BEAR_ID")),
+	@AttributeOverride(name="name", column=@Column(name="BEAR_NAME")),
+	@AttributeOverride(name="cave", column=@Column(name="BEAR_CAVE"))
+})
+public class Bear extends Animal implements Serializable{
 
 	private static final long serialVersionUID = -6062750562012564188L;
 	
-	@Id
-	@Column(name="BEAR_ID")
-	@SequenceGenerator(name="B_SEQ", sequenceName="B_ID_SEQ")
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="B_SEQ")
-	private int id;
+//	@Id
+//	@Column(name="BEAR_ID")
+//	@SequenceGenerator(name="B_SEQ", sequenceName="B_ID_SEQ")
+//	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="B_SEQ")
+//	private int id;
 	
-	@Column(name="BEAR_NAME", nullable=false)
-	private String name;
+
 	
 	@Column(name="BEAR_COLOR")
 	private String color;
 
-	public Bear() {}
 	
-	public Bear(int id, String name, String color) {
+	public Bear() {}
+
+
+	public Bear(String color) {
 		super();
-		this.id = id;
-		this.name = name;
 		this.color = color;
 	}
+	
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public String getColor() {
 		return color;
 	}
 
+
 	public void setColor(String color) {
 		this.color = color;
 	}
 
-	@Override
-	public String toString() {
-		return "Bear [id=" + id + ", name=" + name + ", color=" + color + "]";
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
+	
+	
+	
+
 	
 	
 	
--- Spring/springdemo/src/main/java/com/ex/orm/ORMDao.java
+++ Spring/springdemo/src/main/java/com/ex/orm/ORMDao.java
@@ -1,5 +1,7 @@
 package com.ex.orm;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,19 @@ public class ORMDao {
 	public void buildABear(Bear bear) {
 		Session s = sf.getCurrentSession();
 		s.save(bear);
+	}
+	
+	public List<Bear> getBears(){
+		Session s = sf.getCurrentSession();
+		List<Bear> bears = s.createQuery("from Bear").list();
+		return bears;
+	}
+	
+
+	public List<Bear> getBearsCriteria(){
+		Session s = sf.getCurrentSession();
+		List<Bear> bears = s.createCriteria(Bear.class).list();
+		return bears;
 	}
 	
 }
