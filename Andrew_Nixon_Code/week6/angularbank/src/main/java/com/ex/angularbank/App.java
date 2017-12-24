22c749aab3ae879bbf6238b0e7432ac011553e04
COLLEGE\Andy <adrnix@gmail.com>
2017-12-24 17:55:52 -0500

    initial angbank front end commit

--- Andrew_Nixon_Code/week6/angularbank/src/main/java/com/ex/angularbank/App.java
+++ Andrew_Nixon_Code/week6/angularbank/src/main/java/com/ex/angularbank/App.java
@@ -1,11 +1,13 @@
 package com.ex.angularbank;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
